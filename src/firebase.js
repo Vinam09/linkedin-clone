8f840dfe6d41861d2725f3334ce53183d8aae5bf
Vinam09 <69221071+Vinam09@users.noreply.github.com>
2022-02-18 11:32:53 -0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { initializeApp } from 'firebase/app';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBhQqTR0xRvsI7SsL50xfvtrWKbvyoLajs",
-    authDomain: "linkedin-clone-8958e.firebaseapp.com",
-    projectId: "linkedin-clone-8958e",
-    storageBucket: "linkedin-clone-8958e.appspot.com",
-    messagingSenderId: "874859088042",
-    appId: "1:874859088042:web:4fea1d0a5127cc8497e8d3"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: ",
+    messagingSenderId: "",
+    appId: ""
   };
 
   // const app = initializeApp(firebaseConfig);
